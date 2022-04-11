a6c05caa6ce0e8ceb38ee44fd6bac863ef83257e
Wasif Zulkernine <48087763+Wasifz9@users.noreply.github.com>
2022-04-11 16:38:56 -0400

    Update app_test.py

--- flaskr-tdd/tests/app_test.py
+++ flaskr-tdd/tests/app_test.py
@@ -12,7 +12,7 @@ def client():
     app.config["TESTING"] = True
     app.config["DATABASE"] = BASE_DIR.joinpath(TEST_DB)
     app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{BASE_DIR.joinpath(TEST_DB)}"
-
+    print("test")
     db.create_all()  # setup
     yield app.test_client()  # tests run here
     db.drop_all()  # teardown
